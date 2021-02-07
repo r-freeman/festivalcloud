7af36f48d2e8f22a0b2f75fa073828e8b26b6962
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-07 01:27:28 +0000

    Fixed navigation issues with toolbar

--- utils/toolbar.php
+++ utils/toolbar.php
@@ -1,23 +1,26 @@
+<?php
+require_once 'functions.php';
+define('HOSTNAME', $_SERVER['EBS_HOSTNAME']);
+
+?>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#">FestivalCloud</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <?php
-        require_once 'functions.php';
-        
-        echo '<a class="nav-link" href="/festivalCloud/">Home</a>';
-        echo '<a class="nav-link" href="/festivalCloud/views/festivals/index.php">Festivals</a>';
-        echo '<a class="nav-link" href="/festivalCloud/views/stages/index.php">Stages</a>';
-        echo '<a class="nav-link" href="/festivalCloud/views/shows/index.php">Shows</a>';
-        echo '<a class="nav-link" href="/festivalCloud/views/performers/index.php">Performers</a>';
-        ?>
-      </div>
+    <div class="container">
+        <a class="navbar-brand" href="#">FestivalCloud</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-link" href=<?= HOSTNAME ?>>Home</a>
+                <a class="nav-link" href=<?= HOSTNAME . 'views/festivals' ?>>Festivals</a>
+                <a class="nav-link" href=<?= HOSTNAME . 'views/stages' ?>>Stages</a>
+                <a class="nav-link" href=<?= HOSTNAME . 'views/shows' ?>>Shows</a>
+                <a class="nav-link" href=<?= HOSTNAME . 'views/performers' ?>>Performers</a>
+            </div>
+        </div>
     </div>
-  </div>
 </nav>
 <br>
 <br>
