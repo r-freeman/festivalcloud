c8d6adda66fb5356a740a698ca55db3a543dc7d2
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-07 01:25:38 +0000

    Changed Connections class to use RDS environment variables

--- classes/Connection.php
+++ classes/Connection.php
@@ -1,12 +1,20 @@
 <?php
-class Connection {
 
-    private static $host = "localhost";
-    private static $database = "cloud_festivals_db";
-    private static $username = "root";
-    private static $password = "";
+define('DB_NAME', $_SERVER['RDS_DB_NAME']);
+define('DB_USER', $_SERVER['RDS_USERNAME']);
+define('DB_PASSWORD', $_SERVER['RDS_PASSWORD']);
+define('DB_HOST', $_SERVER['RDS_HOSTNAME']);
 
-    public static function getInstance() {
+class Connection
+{
+
+    private static $database = DB_NAME;
+    private static $username = DB_USER;
+    private static $password = DB_PASSWORD;
+    private static $host = DB_HOST;
+
+    public static function getInstance()
+    {
         $dsn = 'mysql:host=' . Connection::$host . ';dbname=' . Connection::$database;
 
         $connection = new PDO($dsn, Connection::$username, Connection::$password);
