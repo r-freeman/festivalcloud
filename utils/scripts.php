62b5f905bb43fd75e4af8f4d75ada83515d0b409
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-07 01:30:57 +0000

    Changed url for scripts and styles

--- utils/scripts.php
+++ utils/scripts.php
@@ -1,3 +1,3 @@
-<script type="text/javascript" src="/festivalCloud/scripts/jquery-3.5.1.min.js"></script>
-<script type="text/javascript" src="/festivalCloud/scripts/bootstrap.min.js"></script>
-<script type="text/javascript" src="/festivalCloud/scripts/main.js"></script>
+<script type="text/javascript" src="/scripts/jquery-3.5.1.min.js"></script>
+<script type="text/javascript" src="/scripts/bootstrap.min.js"></script>
+<script type="text/javascript" src="/scripts/main.js"></script>
--- utils/styles.php
+++ utils/styles.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="/festivalCloud/styles/bootstrap.min.css">
+<link rel="stylesheet" href="/styles/bootstrap.min.css">
 <!-- <link rel="stylesheet" href="styles/bootstrap-theme.min.css"> -->
-<link rel="stylesheet" href="/festivalCloud/styles/main.css">
+<link rel="stylesheet" href="/styles/main.css">
