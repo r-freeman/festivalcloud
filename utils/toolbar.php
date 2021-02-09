d6f68fdd936e07bfb9b533689272084b900c86b0
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-09 23:28:23 +0000

    Fixed toolbar urls

--- utils/toolbar.php
+++ utils/toolbar.php
@@ -1,9 +1,3 @@
-<?php
-require_once 'functions.php';
-define('HOSTNAME', $_SERVER['EBS_HOSTNAME']);
-
-?>
-
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
         <a class="navbar-brand" href="#">FestivalCloud</a>
@@ -13,11 +7,11 @@ define('HOSTNAME', $_SERVER['EBS_HOSTNAME']);
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link" href=<?= HOSTNAME ?>>Home</a>
-                <a class="nav-link" href=<?= HOSTNAME . 'views/festivals' ?>>Festivals</a>
-                <a class="nav-link" href=<?= HOSTNAME . 'views/stages' ?>>Stages</a>
-                <a class="nav-link" href=<?= HOSTNAME . 'views/shows' ?>>Shows</a>
-                <a class="nav-link" href=<?= HOSTNAME . 'views/performers' ?>>Performers</a>
+                <a class="nav-link" href="/">Home</a>
+                <a class="nav-link" href="/views/festivals">Festivals</a>
+                <a class="nav-link" href="/views/stages">Stages</a>
+                <a class="nav-link" href="/views/shows">Shows</a>
+                <a class="nav-link" href="/views/performers">Performers</a>
             </div>
         </div>
     </div>
