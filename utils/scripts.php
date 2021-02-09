466eddd60fb7c46f78332d900e51a8df73a8b87b
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-09 23:32:58 +0000

    Added S3 urls for static assets

--- utils/scripts.php
+++ utils/scripts.php
@@ -1,3 +1,6 @@
-<script type="text/javascript" src="/scripts/jquery-3.5.1.min.js"></script>
-<script type="text/javascript" src="/scripts/bootstrap.min.js"></script>
-<script type="text/javascript" src="/scripts/main.js"></script>
+<script type="text/javascript"
+        src="https://elasticbeanstalk-us-east-1-596313505871.s3.amazonaws.com/static/scripts/jquery-3.5.1.min.js"></script>
+<script type="text/javascript"
+        src="https://elasticbeanstalk-us-east-1-596313505871.s3.amazonaws.com/static/scripts/bootstrap.min.js"></script>
+<script type="text/javascript"
+        src="https://elasticbeanstalk-us-east-1-596313505871.s3.amazonaws.com/static/scripts/main.js"></script>
--- utils/styles.php
+++ utils/styles.php
@@ -1,3 +1,4 @@
-<link rel="stylesheet" href="/styles/bootstrap.min.css">
+<link rel="stylesheet"
+      href="https://elasticbeanstalk-us-east-1-596313505871.s3.amazonaws.com/static/styles/bootstrap.min.css">
 <!-- <link rel="stylesheet" href="styles/bootstrap-theme.min.css"> -->
-<link rel="stylesheet" href="/styles/main.css">
+<link rel="stylesheet" href="https://elasticbeanstalk-us-east-1-596313505871.s3.amazonaws.com/static/styles/main.css">
